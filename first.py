67565b2c5b8a83267b8ee5ae7f3ade99170dfa6a
unknown <MEGAPOLIS@903-4.luban.mtuci.local>
2024-03-27 18:57:04 +0300

    Небольшой рефактор

--- first.py
+++ first.py
@@ -8,12 +8,11 @@ with open('history_mirror.csv', 'r', encoding='utf-8') as file:
             data.append(temp)
 
 with open('mirror_error.csv', 'w', encoding='utf-8') as file:
-    counter = 0
+    print('date,username', file=file)
     for el in data:
-        fn = el['name'].split()
-        res = fn[0] + ' ' + fn[1][0] + '.' + fn[2][0] + '.'
-        if counter == 0:
-            print(f'Сообщение было зафиксировано: {el["date"]} у пользователя {res}')
-        print(f'Сообщение было зафиксировано: {el["date"]} у пользователя {res}', file=file)
-        counter += 1
+        print(f'{el["date"]},{el["name"]}', file=file)
 
+el = min(data, key=lambda x: (int(x['date'].split('-')[0]), int(x['date'].split('-')[1]), int(x['date'].split('-')[2])))
+fn = el['name'].split()
+res = fn[0] + ' ' + fn[1][0] + '.' + fn[2][0] + '.'
+print(f'Сообщение было зафиксировано: {el["date"]} у пользователя {res}')
